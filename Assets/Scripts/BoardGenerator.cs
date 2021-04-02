d1b658de7f826f803694549eed9866d35114c517
Waldherr Timo <tw086@hdm-stuttgart.de>
2021-04-02 22:55:29 +0200

    added random values to hexagon fields

--- Assets/Scripts/BoardGenerator.cs
+++ Assets/Scripts/BoardGenerator.cs
@@ -28,6 +28,7 @@ public class BoardGenerator : MonoBehaviour
     public GameObject hexagonPrefabWood;
 
     private Stack<GameObject> randomHexStack;
+    private Stack<int> randomNumStack;
 
     private const float r = 0.866f;
     private const float a = 1f;
@@ -40,6 +41,8 @@ public class BoardGenerator : MonoBehaviour
     void Start()
     {
         initalizeRandomHexagons();
+        initializeRandomNumbers();
+
         for (int z = 0; z < gameboardConfig.GetLength(0); z++)
         {
             for (int x = 0; x < gameboardConfig.GetLength(1); x++)
@@ -58,14 +61,22 @@ public class BoardGenerator : MonoBehaviour
                     continue;
                 }
 
+                GameObject newHexagon;
+
                 if (z % 2 == 0)
                 {
-                    Instantiate(whatHexagon, new Vector3(x * s, 0, z / 2f * (d + a)), Quaternion.identity);
+                    newHexagon = Instantiate(whatHexagon, new Vector3(x * s, 0, z / 2f * (d + a)), Quaternion.identity);
                 }
                 else
                 {
-                    Instantiate(whatHexagon, new Vector3(x * s + r, 0, (z - 1) / 2f * (d + a) + offset),
-                        Quaternion.identity);
+                    newHexagon = Instantiate(whatHexagon, new Vector3(x * s + r, 0, (z - 1) / 2f * (d + a) + offset), Quaternion.identity);
+                }
+                
+                // change hexagon numbers
+                if (currentConfig == 2) {
+                    GameObject text = newHexagon.transform.GetChild(1).GetChild(0).gameObject;
+
+                    text.GetComponent<TextMesh>().text = getNextRandomNumber().ToString();
                 }
             }
         }
@@ -81,12 +92,25 @@ public class BoardGenerator : MonoBehaviour
             hexagonPrefabWheet, hexagonPrefabWheet, hexagonPrefabWheet, hexagonPrefabWheet,
             hexagonPrefabWood, hexagonPrefabWood, hexagonPrefabWood, hexagonPrefabWood,
         };
+
         randomHexStack = new Stack<GameObject>(randomHexArray.OrderBy(n => Guid.NewGuid()).ToArray());
     }
+
+    void initializeRandomNumbers()
+    {
+        int[] randomNumArray = new[] {2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12};
+
+        randomNumStack = new Stack<int>(randomNumArray.OrderBy(n => Guid.NewGuid()).ToArray());
+    }
     
-    GameObject getNextRandomHexagon()
+    private GameObject getNextRandomHexagon()
     {
         return randomHexStack.Pop();
+    }
+
+    private int getNextRandomNumber()
+    {
+        return randomNumStack.Pop();
     }
 
     // Update is called once per frame
