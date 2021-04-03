3dfd1c45ea4647435c7fc62aaa4d612345aeeea1
Musse Lena <lm138@hdm-stuttgart.de>
2021-04-03 20:48:04 +0200

    add port objects, ports to scripts, robber object, robber to script

--- Assets/Scripts/BoardGenerator.cs
+++ Assets/Scripts/BoardGenerator.cs
@@ -3,32 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class BoardGenerator : MonoBehaviour
 {
+
+    public GameObject robber;
+    
     public GameObject[] hexagons;
 
     public int[,] gameboardConfig =
     {
-        {0, 0, 1, 1, 1, 1, 0},
-        { 0, 1, 2, 2, 2, 1, 0},
-        {0, 1, 2, 2, 2, 2, 1},
-        { 1, 2, 2, 3, 2, 2, 1},
-        {0, 1, 2, 2, 2, 2, 1},
-        { 0, 1, 2, 2, 2, 1, 0},
-        {0, 0, 1, 1, 1, 1, 0},
+        {0, 0, 4, 1, 4, 1, 0},
+        { 0, 1, 2, 2, 2, 4, 0},
+        {0, 4, 2, 2, 2, 2, 1},
+        { 1, 2, 2, 3, 2, 2, 4},
+        {0, 4, 2, 2, 2, 2, 1},
+        { 0, 1, 2, 2, 2, 4, 0},
+        {0, 0, 4, 1, 4, 1, 0},
     };
-
-    public GameObject hexagonPrefabWater;
+    
     public GameObject hexagonPrefabDesert;
     public GameObject hexagonPrefabBrick;
     public GameObject hexagonPrefabOre;
     public GameObject hexagonPrefabSheep;
-    public GameObject hexagonPrefabWheet;
+    public GameObject hexagonPrefabWheat;
     public GameObject hexagonPrefabWood;
+    
+    public GameObject hexagonPrefabWater;
+    public GameObject hexagonPrefabRandomPort;
+    public GameObject hexagonPrefabBrickPort;
+    public GameObject hexagonPrefabOrePort;
+    public GameObject hexagonPrefabSheepPort;
+    public GameObject hexagonPrefabWheatPort;
+    public GameObject hexagonPrefabWoodPort;
 
     private Stack<GameObject> randomHexStack;
     private Stack<int> randomNumStack;
+    private Stack<GameObject> randomPortHexStack;
 
     private const float r = 0.866f;
     private const float a = 1f;
@@ -41,7 +53,9 @@ public class BoardGenerator : MonoBehaviour
     void Start()
     {
         initalizeRandomHexagons();
+        initalizeRandomPortHexagons();
         initializeRandomNumbers();
+        
 
         for (int z = 0; z < gameboardConfig.GetLength(0); z++)
         {
@@ -55,6 +69,7 @@ public class BoardGenerator : MonoBehaviour
                     1 => hexagonPrefabWater,
                     2 => getNextRandomHexagon(),
                     3 => hexagonPrefabDesert,
+                    4 => getNextRandomPortHexagon(),
                     _ => null,
                 };
                 if (whatHexagon == null) {
@@ -62,15 +77,17 @@ public class BoardGenerator : MonoBehaviour
                 }
 
                 GameObject newHexagon;
+                
 
                 if (z % 2 == 0)
                 {
-                    newHexagon = Instantiate(whatHexagon, new Vector3(x * s, 0, z / 2f * (d + a)), Quaternion.identity);
+                    newHexagon = Instantiate(whatHexagon, new Vector3(x * s, 0, z / 2f * (d + a)), Rotation(currentConfig, z, x));
                 }
                 else
                 {
-                    newHexagon = Instantiate(whatHexagon, new Vector3(x * s + r, 0, (z - 1) / 2f * (d + a) + offset), Quaternion.identity);
+                    newHexagon = Instantiate(whatHexagon, new Vector3(x * s + r, 0, (z - 1) / 2f * (d + a) + offset), Rotation(currentConfig, z, x));
                 }
+                
                 
                 // change hexagon numbers
                 if (currentConfig == 2) {
@@ -78,8 +95,40 @@ public class BoardGenerator : MonoBehaviour
 
                     text.GetComponent<TextMesh>().text = getNextRandomNumber().ToString();
                 }
+                
+                //get position of desert
+                if (whatHexagon == hexagonPrefabDesert)
+                {
+                    whatHexagon.transform.position = newHexagon.transform.position;
+                }
+                
             }
         }
+        
+        //robber
+        Instantiate(robber, hexagonPrefabDesert.transform.position, Quaternion.identity);
+    }
+
+    Quaternion Rotation(int currentConfig, int z, int x)
+    {
+        //Ports need to be rotated
+        if (currentConfig == 4)
+        {
+            switch (z, x)
+            {
+                case (0,2): return Quaternion.Euler(0,120,0);
+                case (0,4): return Quaternion.Euler(0,60,0);
+                case (2,1): return Quaternion.Euler(0,180,0);
+                case (4,1):
+                case (6,2): 
+                    return Quaternion.Euler(0,-120,0);
+                case (6,4): 
+                case (5,5):    
+                    return Quaternion.Euler(0,-60,0);
+            }
+            
+        }
+        return Quaternion.identity;
     }
 
     void initalizeRandomHexagons()
@@ -89,11 +138,26 @@ public class BoardGenerator : MonoBehaviour
             hexagonPrefabBrick, hexagonPrefabBrick, hexagonPrefabBrick,
             hexagonPrefabOre, hexagonPrefabOre, hexagonPrefabOre,
             hexagonPrefabSheep, hexagonPrefabSheep, hexagonPrefabSheep, hexagonPrefabSheep,
-            hexagonPrefabWheet, hexagonPrefabWheet, hexagonPrefabWheet, hexagonPrefabWheet,
+            hexagonPrefabWheat, hexagonPrefabWheat, hexagonPrefabWheat, hexagonPrefabWheat,
             hexagonPrefabWood, hexagonPrefabWood, hexagonPrefabWood, hexagonPrefabWood,
         };
 
         randomHexStack = new Stack<GameObject>(randomHexArray.OrderBy(n => Guid.NewGuid()).ToArray());
+    }
+    
+    void initalizeRandomPortHexagons()
+    {
+        GameObject[] randomHexArray = new[]
+        {
+            hexagonPrefabRandomPort, hexagonPrefabRandomPort, hexagonPrefabRandomPort, hexagonPrefabRandomPort,
+            hexagonPrefabBrickPort,
+            hexagonPrefabOrePort,
+            hexagonPrefabSheepPort,
+            hexagonPrefabWheatPort,
+            hexagonPrefabWoodPort
+        };
+
+        randomPortHexStack = new Stack<GameObject>(randomHexArray.OrderBy(n => Guid.NewGuid()).ToArray());
     }
 
     void initializeRandomNumbers()
@@ -106,6 +170,11 @@ public class BoardGenerator : MonoBehaviour
     private GameObject getNextRandomHexagon()
     {
         return randomHexStack.Pop();
+    }
+    
+    private GameObject getNextRandomPortHexagon()
+    {
+        return randomPortHexStack.Pop();
     }
 
     private int getNextRandomNumber()
