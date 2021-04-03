377018411cac152dcb09b42e55f2b94f66c62eb5
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2021-04-04 01:08:26 +0200

    Added a test UI in TestScene, added a Script for Playtime counter

---
+++ Assets/Scripts/TextPlaytime.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TextPlaytime : MonoBehaviour
+{
+    public Text timerText;
+    private DateTime dt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dt = System.DateTime.Now;
+        Debug.Log("Current DateTime.Now: " + dt);
+    }
+
+    
+
+    // Update is called on every frame
+    void Update()
+    {
+        timerText.text = "Playtime: " + string.Format("{0:hh\\:mm\\:ss}", DateTime.Now - dt);
+    }
+}
