386c6b2131db5f25e9eb7a6e47920ba1c218293f
Andrea Feurer <andrea@feurer-net.de>
2021-05-02 15:13:03 +0200

    🚧 begin clientToServerCommunication implementation

---
+++ Assets/Scripts/Networking/ClientRequest.cs
@@ -0,0 +1,46 @@
+﻿using Resource;
+
+namespace Networking
+{
+    public class ClientRequest : ClientToServerCommunication
+    {
+        public void requestBeginRound(ClientToServerCommunication.acceptCallback acceptCallback, ClientToServerCommunication.rejectCallback rejectCallback)
+        {
+            // alle Daten sammeln
+            
+            // daten verpacken
+            
+            // daten senden
+            
+            // accept oder reject verarbeiten
+            
+            throw new System.NotImplementedException();
+        }
+
+        public void requestTradeBank(RESOURCE offer, RESOURCE expectation, ClientToServerCommunication.acceptCallback acceptCallback,
+            ClientToServerCommunication.rejectCallback rejectCallback)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void requestBuild(BUYABLES type, int x, int y, ClientToServerCommunication.acceptCallback acceptCallback, ClientToServerCommunication.rejectCallback rejectCallback)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void requestBuyDevelopement(ClientToServerCommunication.acceptCallback acceptCallback, ClientToServerCommunication.rejectCallback rejectCallback)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void requestPlayDevelopement(ClientToServerCommunication.acceptCallback acceptCallback, ClientToServerCommunication.rejectCallback rejectCallback)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void requestEndTurn(ClientToServerCommunication.acceptCallback acceptCallback, ClientToServerCommunication.rejectCallback rejectCallback)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
