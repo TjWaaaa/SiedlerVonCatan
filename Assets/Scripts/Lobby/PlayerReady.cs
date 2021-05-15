636ee8947c7a8c1dd2aec0fd649a3088351bec08
Geupel Simon <sg184@hdm-stuttgart.de>
2021-05-15 19:30:43 +0200

    :bug: fixed issue with client echoing the ready status because of a change in the servers ready status

--- Assets/Scripts/Lobby/PlayerReady.cs
+++ Assets/Scripts/Lobby/PlayerReady.cs
@@ -1,8 +1,9 @@
 using Networking;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class PlayerReady : MonoBehaviour
+public class PlayerReady : MonoBehaviour, IPointerClickHandler
 {
     private readonly ClientRequest clientRequest = new ClientRequest();
     private bool oldReadyStatus;
@@ -14,12 +15,17 @@ public class PlayerReady : MonoBehaviour
         toggleComponent = this.GetComponent<Toggle>();
     }
 
-    private void Update()
+    // private void Update()
+    // {
+    //     if (oldReadyStatus != toggleComponent.isOn) // if ready status was updated --> send to server
+    //     {
+    //         oldReadyStatus = toggleComponent.isOn;
+    //         clientRequest.requestPlayerReady(toggleComponent.isOn);
+    //     }
+    // }
+
+    public void OnPointerClick(PointerEventData eventData)
     {
-        if (oldReadyStatus != toggleComponent.isOn)
-        {
-            oldReadyStatus = toggleComponent.isOn;
-            clientRequest.requestPlayerReady(toggleComponent.isOn);
-        }
+        clientRequest.requestPlayerReady(toggleComponent.isOn);
     }
 }
