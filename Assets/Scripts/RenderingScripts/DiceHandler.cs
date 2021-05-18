a7467039559531b9986144e8eee121561d60da3f
Maximilian Dolbaum <md127@hdm-stuttgart.de>
2021-05-18 23:57:00 +0200

    adding 2 scripts to handle the dice animations
    Currently if you press space the animation is started with a fixed number input, this will later be replaced by the gameControllers call

---
+++ Assets/Scripts/RenderingScripts/DiceHandler.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceHandler : MonoBehaviour
+{
+    private int diceNumber;
+
+    private Animator dice;
+
+    void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        dice = gameObject.GetComponent<Animator>();
+        dice.enabled = false;
+    }
+    public void updateDiceNumber(int number)
+    {
+        diceNumber = number;
+    }
+
+    public void checkForOne()
+    {
+        if (diceNumber == 1)
+        {
+            checkedCorrect();
+        }
+    }
+    public void checkForTwo()
+    {
+        if (diceNumber == 2)
+        {
+            checkedCorrect();
+        }
+    }
+    public void checkForThree()
+    {
+        if (diceNumber == 3)
+        {
+            checkedCorrect();
+        }
+    }
+    public void checkForFour()
+    {
+        if (diceNumber == 4)
+        {
+            checkedCorrect();
+        }
+    }
+    public void checkForFive()
+    {
+        if (diceNumber == 5)
+        {
+            checkedCorrect();
+        }
+    }
+    public void checkForSix()
+    {
+        if (diceNumber == 6)
+        {
+            checkedCorrect();
+        }
+    }
+
+    private void checkedCorrect()
+    {
+        dice.enabled = false;
+        diceNumber = -1;
+    }
+}
--- Assets/Scripts/RenderingScripts/RenderRollDices.cs
+++ Assets/Scripts/RenderingScripts/RenderRollDices.cs
@@ -4,23 +4,43 @@ using UnityEngine;
 
 public class RenderRollDices : MonoBehaviour
 {
-    public Animation Dice1;
-    public Animation Dice2;
+    private DiceHandler Dice1;
+    private DiceHandler Dice2;
+    private int[] diceNumbers = {-1,-1};
+
     // Start is called before the first frame update
     void Start()
     {
-
+        StartCoroutine(TimeYield());
+        DiceHandler[] diceDiceHandlers = GetComponentsInChildren<DiceHandler>();
+        Debug.Log(diceDiceHandlers.Length);
+        Dice1 = diceDiceHandlers[0];
+        Dice2 = diceDiceHandlers[1];
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("space pressed");
+            renderRollDices(new int[] {1,4});
+        }
     }
 
-    public void renderRollDices(int firstNumber, int secondNumber)
+    public void renderRollDices(int[] numbers)
     {
-        Dice1.enabled = true;
-        Dice2.enabled = true;
+        Dice1.GetComponent<Animator>().enabled = true;
+        Dice2.GetComponent<Animator>().enabled = true;
+        diceNumbers = numbers;
+        TimeYield();
     }
+
+    public IEnumerator TimeYield()
+    {
+        yield return new WaitForSeconds(2);
+        Dice1.updateDiceNumber(diceNumbers[0]);
+        Dice2.updateDiceNumber(diceNumbers[1]);
+    }
+
 }
