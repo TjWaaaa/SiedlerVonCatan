4bee0cf7ec72fbacd7c8cd5ff2afce5d0221adcc
lenzge <lm138@hdm-stuttgart.de>
2021-05-24 17:57:59 +0200

    Player as Prefab in AllPlayers, instantiate prefabs in code doesnt work yet

--- Assets/Scripts/UI/PlayerRepresentation.cs
+++ Assets/Scripts/UI/PlayerRepresentation.cs
@@ -6,15 +6,14 @@ namespace UI
 {
     public class PlayerRepresentation : MonoBehaviour
     {
-        private PrefabFactory prefabFactory;
+        //private PrefabFactory prefabFactory;
         private GameObject allPlayers;
         
-        GameObject playerRepresentation_2;
         
         public void Start()
         {
             allPlayers = GameObject.Find("AllPlayers");
-            //GameObject playerRepresentation_1 = playerRepresentation_1 = prefabFactory.getPrefab(PREFABS.PLAYER_REPRESENTATION, allPlayers.transform);
+            
             
             
             
