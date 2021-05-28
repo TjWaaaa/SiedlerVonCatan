204a04fda733387725ed6b8b2971374e8348f4ab
hash-key <81805005+hash-key@users.noreply.github.com>
2021-05-28 13:31:28 +0200

    fixed board generation

--- Assets/Scripts/Board/BoardGenerator.cs
+++ Assets/Scripts/Board/BoardGenerator.cs
@@ -28,9 +28,9 @@ public class BoardGenerator : MonoBehaviour
 
                 int offsetX = row switch
                 {
-                    0 => 2,
-                    1 => 1,
-                    2 => 1,
+                    0 => -1,
+                    1 => -1,
+                    2 => 0,
                     3 => 0,
                     4 => 1,
                     5 => 1,
